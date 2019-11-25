7232f42ae50a0262ba3c8731663f29bae57bbc63
NathanBodet <bbodetnathan@gmail.com>
2019-11-25 12:06:22 +0100

    modification du tableau
    on peut modifier le tableau avec la console !

--- Main.c
+++ Main.c
@@ -1,39 +1,84 @@
 #include <stdio.h>
 #include <string.h>
+
+#define TAILLETAB 4
+int priorites[TAILLETAB]; //Table d'allocation des priorités (choisie avec ces pourcentages-là par défaut)
+
+//affiche le tableau d'allocation des priorités
+void afficheTableau(){
+	printf("**********************************************\nTableau des priorités:\n");
+	printf("———————————————————————————————————\n|numéro de priorité | pourcentage |\n");
+	for (int i = 0; i < 4; ++i)
+	{
+		printf("| %d                 | %d          |\n",i,priorites[i]);
+	}
+	printf("———————————————————————————————————\n");
+	printf("**********************************************\n");
+}
+
+void modifierTableau(){
+	//on affiche le tableau si l'utilisateur a choisi -t
+	afficheTableau();
+	char reponse[8];
+
+	printf("\nVoulez-vous modifier le tableau des priorités? (o/n)\n");
+	fflush(stdout);
+
+	//entrée de l'utilisateur, test de réponse pour qu'on ne puisse entrer que n ou o
+	//strcmp compare deux chaines de caractères (renvoie 1 si faux, 0 si vrai)
+	while(strcmp(reponse,"o") && strcmp(reponse,"n")){
+		scanf("%s",reponse);//on récupère l'input de l'utilisateur
+
+		if(strcmp(reponse,"o") && strcmp(reponse,"n")){
+			printf("entree : %s\n",reponse);
+			printf("entrée incorrecte : entrez o pour oui et n pour non\n");
+			fflush(stdout);
+		}
+	}
+
+	if(strcmp(reponse,"o") == 0){
+		//choix de modification par l'utilisateur
+		printf("Entrez les nouvelles valeurs des pourcentages :\n");
+		int prioriteTemp[TAILLETAB]; // tableau temporaire pour stocker les pourcentages
+
+		int resultatVrai = 0; //sert de condition pour vérifier si les valeurs entrées sont bonnes
+		while(resultatVrai == 0){
+
+			for (int i = 0; i < TAILLETAB; i+=1)
+			{
+				printf("priorité %d : pourcentage? ",i);
+				scanf("%s",reponse);
+				prioriteTemp[i] = atoi(reponse);
+			}
+			//test si les valeurs sont correctes. Si oui, on applique les modifications
+			/*a adapter à TAILLETAB*/
+			if(prioriteTemp[0] + prioriteTemp[1] + prioriteTemp[2] + prioriteTemp[3] == 100){
+				resultatVrai = 1;
+				for (int i = 0; i < TAILLETAB; i+=1){
+					priorites[i] = prioriteTemp[i];
+				}
+			} else {
+				printf("Les valeurs entrées ne correspondent pas à des pourcentages valides.\n");
+			}
+		}
+		afficheTableau();
+	}
+}
 int main(int argc, char const *argv[])
 {
-	char reponse[8];
-	char *o = "o";
-	char *n = "n";
-	//Table d'allocation des priorités (choisie avec ces pourcentages-là par défaut)
-	int priorites[4] = {40,30,20,10};
-
-	//Si il y a un argument, on affiche la table d'allocation (il faut tester pour que ça soit l'argrument 't', j'ai pas encore fait)
-	if(argc == 2){
-		printf("****************************\nTableau des priorités:\n");
-		printf("———————————————————————————————————\n|numéro de priorité | pourcentage |\n");
-		for (int i = 0; i < 4; ++i)
-		{
-			printf("| %d                 | %d          |\n",i,priorites[i]);
-		}
-		printf("———————————————————————————————————\n");
-
-		printf("\nVoulez-vous modifier le tableau des priorités? (o/n)\n");
-		fflush(stdout);
-		//entrée de l'utilisateur, test de réponse qui marche pas encore :/
-		/*while(reponse != o && reponse != n){
-			scanf("%s",reponse);//on récupère l'input de l'utilisateur
-
-			if(reponse != o && reponse != n){
-				printf("entrée incorrecte : entrez o pour oui et n pour non\n");
-				fflush(stdout);
-			}
-		}*/
-		
-
-		return 0;
+	if(argc == 1){
+		//différentes options de lancement : pour l'instant il n'y a que -t
+		printf("Vous n'avez pas entré d'options. Options disponibles :\n -t : afficher ou modifier la table d'allocation CPU\n");
+		return(1);
 	}
-	
-	
+	priorites[0] = 40;
+	priorites[1] = 30;
+	priorites[2] = 20;
+	priorites[3] = 10;
+
+	//Si il y a l'argument '-t', on affiche la table d'allocation CPU
+	if(argc == 2 && strcmp ("-t", argv[1]) == 0){
+		modifierTableau();
+	}
 	return 0;
 }
